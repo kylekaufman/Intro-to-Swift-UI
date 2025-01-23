5574b60bdf13de297dc43181415fbb4e00c4535d
kylekaufman <ktk33@case.edu>
2025-01-23 12:29:28 -0500

    Initial Commit

---
+++ Intro to Swift UI/Intro_to_Swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Intro_to_Swift_UIApp.swift
+//  Intro to Swift UI
+//
+//  Created by Kyle Kaufman on 1/23/25.
+//
+
+import SwiftUI
+
+@main
+struct Intro_to_Swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
