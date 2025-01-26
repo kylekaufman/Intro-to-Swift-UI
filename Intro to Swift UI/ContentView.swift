f4418922b4f286c2f763eb0464c654b7100cc350
kylekaufman <ktk33@case.edu>
2025-01-25 19:27:01 -0500

    added message bubbles

--- Intro to Swift UI/ContentView.swift
+++ Intro to Swift UI/ContentView.swift
@@ -8,9 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var messages = [String]()
+    @State var message = ""
     var body: some View {
         VStack {
-            Text("Testing123")
+            ScrollView {
+                Section {
+                    ForEach(messages, id:\.self) { message in
+                        Text(message)
+                            .padding(10)
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(15)
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                            .padding(.leading, 50)
+                            .padding(.horizontal)
+                    }
+                    .padding(.vertical, 5)
+                }
+            }
+            .refreshable {
+                messages.removeAll()
+            }
+            HStack {
+                TextField("Message", text: $message)
+                Button(action: {
+                    messages.append(message)
+                    message = ""
+                }) {
+                    Image(systemName: "paperplane")
+                }
+            }
         }
         .padding()
     }
