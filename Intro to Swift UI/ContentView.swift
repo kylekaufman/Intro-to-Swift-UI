ec28275c42428d91c07327844740c26465e53942
kylekaufman <ktk33@case.edu>
2025-01-25 20:02:33 -0500

    switch to using struct instead of string

--- Intro to Swift UI/ContentView.swift
+++ Intro to Swift UI/ContentView.swift
@@ -7,22 +7,42 @@
 
 import SwiftUI
 
+struct Message: Identifiable {
+    var id: UUID
+    var message: String
+    var date: Date
+
+    init(message: String) {
+        self.id = UUID()
+        self.message = message
+        self.date = Date()
+    }
+}
+
+struct MessageView: View {
+    var message: Message
+    var body: some View {
+        Text(message.message)
+            .padding(10)
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .cornerRadius(15)
+            .frame(maxWidth: .infinity, alignment: .trailing)
+            .padding(.leading, 50)
+            .padding(.horizontal)
+            .padding(.vertical, 5)
+    }
+}
+
 struct ContentView: View {
-    @State var messages = [String]()
+    @State var messages = [Message]()
     @State var message = ""
     var body: some View {
         VStack {
             ScrollView {
                 Section {
-                    ForEach(messages, id:\.self) { message in
-                        Text(message)
-                            .padding(10)
-                            .background(Color.blue)
-                            .foregroundColor(.white)
-                            .cornerRadius(15)
-                            .frame(maxWidth: .infinity, alignment: .trailing)
-                            .padding(.leading, 50)
-                            .padding(.horizontal)
+                    ForEach(messages) { msg in
+                        MessageView(message: msg)
                     }
                     .padding(.vertical, 5)
                 }
@@ -33,7 +53,7 @@ struct ContentView: View {
             HStack {
                 TextField("Message", text: $message)
                 Button(action: {
-                    messages.append(message)
+                    messages.append(Message(message: message))
                     message = ""
                 }) {
                     Image(systemName: "paperplane")
